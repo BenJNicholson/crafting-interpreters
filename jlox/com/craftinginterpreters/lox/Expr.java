5fb4cdb48b9adf52ccacc8e330a9bcee6267fd00
Ben Nicholson <155940618+BenJNicholson@users.noreply.github.com>
2025-03-21 19:29:49 -0400

    Ch 8 progress

--- jlox/com/craftinginterpreters/lox/Expr.java
+++ jlox/com/craftinginterpreters/lox/Expr.java
@@ -3,21 +3,33 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 
 abstract class Expr {
-  interface Visitor<R> {
+ interface Visitor<R> {
+    R visitAssignExpr(Assign expr);
     R visitBinaryExpr(Binary expr);
-
     R visitGroupingExpr(Grouping expr);
-
     R visitLiteralExpr(Literal expr);
-
     R visitUnaryExpr(Unary expr);
+    R visitVariableExpr(Variable expr);
   }
+ static class Assign extends Expr {
+    Assign(Token name, Expr value) {
+        this.name = name;
+        this.value = value;
+    }
 
-  static class Binary extends Expr {
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitAssignExpr(this);
+    }
+
+    final Token name;
+    final Expr value;
+  }
+ static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
-      this.left = left;
-      this.operator = operator;
-      this.right = right;
+        this.left = left;
+        this.operator = operator;
+        this.right = right;
     }
 
     @Override
@@ -29,10 +41,9 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
-
-  static class Grouping extends Expr {
+ static class Grouping extends Expr {
     Grouping(Expr expression) {
-      this.expression = expression;
+        this.expression = expression;
     }
 
     @Override
@@ -42,10 +53,9 @@ abstract class Expr {
 
     final Expr expression;
   }
-
-  static class Literal extends Expr {
+ static class Literal extends Expr {
     Literal(Object value) {
-      this.value = value;
+        this.value = value;
     }
 
     @Override
@@ -55,11 +65,10 @@ abstract class Expr {
 
     final Object value;
   }
-
-  static class Unary extends Expr {
+ static class Unary extends Expr {
     Unary(Token operator, Expr right) {
-      this.operator = operator;
-      this.right = right;
+        this.operator = operator;
+        this.right = right;
     }
 
     @Override
@@ -69,6 +78,18 @@ abstract class Expr {
 
     final Token operator;
     final Expr right;
+  }
+ static class Variable extends Expr {
+    Variable(Token name) {
+        this.name = name;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVariableExpr(this);
+    }
+
+    final Token name;
   }
 
   abstract <R> R accept(Visitor<R> visitor);
