9faf8e257b967b249b711e6797f91a00a635d4dc
Ben Nicholson <155940618+BenJNicholson@users.noreply.github.com>
2025-03-21 23:13:00 -0400

    through end of Ch 8

--- jlox/com/craftinginterpreters/tool/GenerateAst.java
+++ jlox/com/craftinginterpreters/tool/GenerateAst.java
@@ -21,6 +21,7 @@ public class GenerateAst {
                 "Variable : Token name"));
 
         defineAST(outputDir, "Stmt", Arrays.asList(
+                "Block      : List<Stmt> statements",
                 "Expression : Expr expression",
                 "Print      : Expr expression",
                 "Var        : Token name, Expr initializer"));
